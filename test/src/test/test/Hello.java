0b023be444a8c5c3d0360e22de37f5b942571e6d
jasontommy <814499185@qq.com>
2016-05-15 02:01:32 +0800

    user2:移动Util.java

--- test/src/test/test/Hello.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class Hello {
-	public static void main(String []args){
-		System.out.println("ÄãºÃÊÀ½ç");
-		System.out.println("hhhhh");
-		System.out.println("hello the world!");
-	}
-}
