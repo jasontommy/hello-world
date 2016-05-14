a6385f99be1cf28005ec17e5be40b2aa620727a5
jasontommy <814499185@qq.com>
2016-05-15 02:14:19 +0800

    user1:删除多余的打印

--- test/src/test/Add.java
+++ test/src/test/Add.java
@@ -2,9 +2,6 @@ package test;
 
 public class Add {
 	public static int add(int a, int b) {
-		System.out.println("asdfasdfasdfasdf");
-		System.out.println("ÔõÃ´À²");
-		System.out.println("add");
 		System.out.println("public");//ÄãºÃ
 		return (a + b);
 	}
