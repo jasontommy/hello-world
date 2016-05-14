bc83b4a09fe7127d3e2f49a5a916d5f00e530aa6
jasontommy <814499185@qq.com>
2016-05-15 00:02:29 +0800

    新建文件Minus.java

--- test/src/test/test/Add.java
+++ test/src/test/test/Add.java
@@ -2,6 +2,7 @@ package test;
 
 public class Add {
 	public static int add(int a, int b) {
+		System.out.println("asdfasdfasdfasdf");
 		System.out.println("ÔõÃ´À²");
 		System.out.println("add");
 		System.out.println("public");//ÄãºÃ
---
+++ test/src/test/test/Minus.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Minus {
+	private String a;
+	public Minus(String a){
+		this.a = a;
+	}
+	
+	public String getA(){
+		return a;
+	}
+}
